19fbe7369ef853b43720a15c82252a65a748dfc1
muhibqosim@gmail.com <muhibqosim@gmail.com>
2025-03-25 10:13:39 +0100

    commit

--- js/solace.js
+++ js/solace.js
@@ -128,4 +128,14 @@ typeWriter();*/
   });
 
 
+
+
+
+
+      //copyright
+      document.addEventListener('DOMContentLoaded', function() {
+        var currentYear = new Date().getFullYear();
+        document.getElementById('copyright-year').textContent = currentYear;
+      });
+      
       
