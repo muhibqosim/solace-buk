85239af6e665bbad95480a97b6e8fbd0a8061b23
muhibqosim@gmail.com <muhibqosim@gmail.com>
2025-04-01 12:31:58 +0100

    commit

--- js/solace.js
+++ js/solace.js
@@ -159,7 +159,7 @@ function updateGallery() {
         let img = document.createElement("img");
         img.src = imageUrl;
         img.alt = "Gallery Image";
-        img.className = "w-full h-92 object-cover flex-shrink-0";
+        img.className = "w-full h-94 object-cover flex-shrink-0";
         img.style.display = index === 0 ? "block" : "none";
         slider.appendChild(img);
     });
